e5577c726836b2bedb714701dc6fc63d484fc172
NataliaFomDev <nataliafominykh4@gmail.com>
2023-09-24 16:44:18 +0600

    feat: fix duration progress (Sun, Sep 24, 2023 16:44:32 PM)

--- audio-player/script.js
+++ audio-player/script.js
@@ -75,13 +75,6 @@ function updateTimeDisplay() {
     }
 }
 
-audio.addEventListener('loadedmetadata', () => {
-    const duration = audio.duration;
-    if (!isNaN(duration)) {
-        durationDisplay.textContent = formatTime(duration);
-    }
-});
-
 // Обновляем время при изменении currentTime аудиоплеера
 audio.addEventListener('timeupdate', updateTimeDisplay);
 // Устанавливаем начальное время (0:00)
@@ -91,6 +84,17 @@ updateTimeDisplay();
 progress.addEventListener('input', () => {
     const newTime = (progress.value / 100) * audio.duration;
     audio.currentTime = newTime;
+});
+
+// Обновляем положение бегунка прогресса при событии timeupdate
+audio.addEventListener('timeupdate', () => {
+    const currentTime = audio.currentTime;
+    const duration = audio.duration;
+    
+    if (!isNaN(duration)) {
+        // Обновляем значение бегунка прогресса в соответствии с текущим временем
+        progress.value = (currentTime / duration) * 100;
+    }
 });
 
 // Обработчик события клика по кнопке "Play"
@@ -126,7 +130,6 @@ forthButton.addEventListener('click', () => {
     updateTimeDisplay();
     playButton.style.display = 'none';
     pauseButton.style.display = 'inline-block';
-    progress.value = 0;
 })
 
 // Обработчик события перехода к предыдущей композиции
@@ -136,5 +139,8 @@ backButton.addEventListener('click', () => {
     updateTimeDisplay();
     playButton.style.display = 'none';
     pauseButton.style.display = 'inline-block';
-    progress.value = 0;
 });
+
+window.onload = function() {
+    console.log("Вёрстка (10 / 10) \nвёрстка аудиоплеера: есть кнопка Play/Pause, кнопки 'Вперёд' и 'Назад' для пролистывания аудиотреков, прогресс-бар, отображается название и автор трека (5 / 5) \nв футере приложения есть ссылка на гитхаб автора приложения, год создания приложения, логотип курса со ссылкой на курс (5 / 5) \nКнопка Play/Pause (10 / 10) \nесть кнопка Play/Pause, при клике по которой можно запустить или остановить проигрывание аудиотрека (5 / 5) \nвнешний вид и функционал кнопки Play/Pause изменяется в зависимости от того, проигрывается ли в данный момент аудиотрек (5 / 5) \nПри кликах по кнопкам 'Вперёд' и 'Назад' переключается проигрываемый аудиотрек. Аудиотреки пролистываются по кругу - после последнего идёт первый (10 / 10)\nПри смене аудиотрека меняется изображение - обложка аудиотрека (10 / 10)\nПрогресс-бар отображает прогресс проигрывания текущего аудиотрека. При перемещении ползунка вручную меняется текущее время проигрывания аудиотрека (5 / 10)\nОтображается продолжительность аудиотрека и его текущее время проигрывания (10 / 10)\nОчень высокое качество оформления приложения и/или дополнительный не предусмотренный в задании функционал, улучшающий качество приложения (10 / 10)\nвысокое качество оформления приложения предполагает собственное оригинальное оформление равное или отличающееся в лучшую сторону по сравнению с демо");
+};
